c70d8814ee0bafd7deafcacbfd126889a2fe7a6d
cybuildguild <macn51@proton.me>
2026-03-22 11:28:39 -0400

    Write Instructions for Smart Contract

--- smart_contract_dir/contracts/agent_smart_contract.sol
+++ smart_contract_dir/contracts/agent_smart_contract.sol
@@ -1,3 +1,7 @@
+//This will be a logging smart contract
+//It will package the actions of the python file agentic_model,
+//  and store them
+
 pragma solidity >=0.5.0 < 0.9.0;
 
 contract agentic_smart_contract 
