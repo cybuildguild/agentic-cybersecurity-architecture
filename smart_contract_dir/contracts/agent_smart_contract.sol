9e4ab8a65a5eb1bbdacd8a6a054801c059ad2ddb
cybuildguild <macn51@proton.me>
2026-03-22 15:39:45 -0400

    Added an Address to Store and Map Records

--- smart_contract_dir/contracts/agent_smart_contract.sol
+++ smart_contract_dir/contracts/agent_smart_contract.sol
@@ -11,7 +11,7 @@ contract agentic_smart_contract
     // Declare a new complex type which will
     // be used for variables later.
     // It will represent a record of a single 
-    // agent's actions
+    // agent's actions.
     struct Record {
         // The following are string types as that is 
         // the return type of those functions in
@@ -22,6 +22,12 @@ contract agentic_smart_contract
         uint timestamp;
     }
 
+    address public overseer;
+
+    // This declares a state variable that stores 
+    // a 'Record' struct for each possible address.
+    mapping(address => Record) public records;
+    
     function packaging_actions() public pure returns (string memory)
     {
         return '';
