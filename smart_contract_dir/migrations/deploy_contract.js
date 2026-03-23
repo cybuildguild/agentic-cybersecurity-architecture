24fc4243b7be3503be0b64d4b0abf314e269b55c
cybuildguild <macn51@proton.me>
2026-03-22 20:13:12 -0400

    Modify deploy_contract to Return Deployer

--- smart_contract_dir/migrations/deploy_contract.js
+++ smart_contract_dir/migrations/deploy_contract.js
@@ -1,5 +1,5 @@
-var agentic_smart_contract = artifacts.require("agentic_smart_contract");
+const agentic_smart_contract = artifacts.require("agentic_smart_contract");
 
 module.exports = function(deployer) {
-    deployer.deploy(agentic_smart_contract);
+    return deployer.deploy(agentic_smart_contract);
 };
